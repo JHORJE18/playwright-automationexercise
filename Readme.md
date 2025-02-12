c366322b2f5a962f208a6c07b5734fcffe497e69
Jorge L. <wiijlg@hotmail.com>
2025-02-12 14:11:33 +0100

    Caso de prueba #12

--- Readme.md
+++ Readme.md
@@ -23,13 +23,13 @@ Este proyecto cubre los siguientes casos de prueba disponibles en Automation Exe
 9. Buscar producto
 10. Verificar suscripción en la página de inicio
 11. Verificar suscripción en la página del carrito
+12. Agregar productos al carrito
 
 25. Verificar el desplazamiento hacia arriba con el botón de "Flecha" y la funcionalidad de desplazamiento hacia abajo
 26. Verificar el desplazamiento hacia arriba sin el botón de "Flecha" y la funcionalidad de desplazamiento hacia abajo
 
 ### Casos de prueba en desarrollo 🚧
 
-12. Agregar productos al carrito
 13. Verificar cantidad de productos en el carrito
 14. Realizar pedido: Registrarse durante el pago
 15. Realizar pedido: Registrarse antes del pago
--- tests/PruebasTienda.spec.ts
+++ tests/PruebasTienda.spec.ts
@@ -11,4 +11,37 @@ test.describe('Casos de prueba [12-24] - Pruebas de gestión de usuario', () => 
         await checkPaginaInicio(page);
     })
 
+    test('Caso de prueba #12 - Agregar productos al carrito', async ({ page }) => {
+        await page.getByRole('link', { name: ' Products' }).click();
+        const listadoProductos = await page.locator('.product-image-wrapper');
+
+        // Añadir al carrito primer producto
+        const primerProducto = await listadoProductos.nth(0).locator('.productinfo');
+        const precioPrimerProducto = await primerProducto.locator('h2').textContent();
+        await primerProducto.getByText('Add to cart').click();
+
+        await page.getByRole('button', { name: 'Continue Shopping' }).click();
+
+        // Añadir segundo producto
+        const segundoProducto = await listadoProductos.nth(1).locator('.productinfo');
+        const precioSegundoProducto = await segundoProducto.locator('h2').textContent();
+        await segundoProducto.getByText('Add to cart').click();
+
+        await page.getByRole('link', { name: 'View Cart' }).click();
+
+        // Verificar productos añadidos
+        const listadoProductosCarrito = await page.locator('tbody').getByRole('row');
+        await expect(await listadoProductosCarrito.count()).toBe(2);
+
+        // Verifica producto #1
+        await expect(listadoProductosCarrito.first().locator('.cart_price p')).toContainText(precioPrimerProducto!)
+        await expect(listadoProductosCarrito.first().locator('.cart_quantity button')).toContainText('1');
+        await expect(listadoProductosCarrito.first().locator('.cart_total p')).toContainText(precioPrimerProducto!);
+
+        // Verifica producto #2
+        await expect(listadoProductosCarrito.last().locator('.cart_price p')).toContainText(precioSegundoProducto!)
+        await expect(listadoProductosCarrito.last().locator('.cart_quantity button')).toContainText('1');
+        await expect(listadoProductosCarrito.last().locator('.cart_total p')).toContainText(precioSegundoProducto!);
+    })
+
 })
